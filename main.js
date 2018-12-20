b9a8950a3a25c21d8463cf973975ca813044828c
Karolina <zygawska.karolina@gmail.com>
2018-12-20 16:48:20 +0100

    1

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
    var mymap = L.map('mymap',
                     {
-                     center: [52.1, 21.0], 
-                     zoom: 10,
+                     center: [50.02, 22.0], 
+                     zoom: 12,
                      zoomControl:true,
                      attributionControl:false
                     }
@@ -36,6 +36,105 @@ $(document).ready(function(){
     } 
     );
     
+    var lyrPRGWody_liniowe = L.tileLayer.wms('http://localhost:8080/geoserver/ATE_Workspace/wms',
+                    {
+                    layers: 'Raster',
+                    format:'image/png',
+                    transparent: 'true',
+                    version:'1.1.1'
+    } 
+    );
+    
+    var lyrPRGWody_powierz = L.tileLayer.wms('http://localhost:8080/geoserver/ATE_Workspace/wms',
+                    {
+                    layers: 'Raster',
+                    format:'image/png',
+                    transparent: 'true',
+                    version:'1.1.1'
+    } 
+    );
+    
+    var lyrPRG_Drogi = L.tileLayer.wms('http://localhost:8080/geoserver/ATE_Workspace/wms',
+                    {
+                    layers: 'Raster',
+                    format:'image/png',
+                    transparent: 'true',
+                    version:'1.1.1'
+    } 
+    );
+    
+    var lyrPRG_Kolej = L.tileLayer.wms('http://localhost:8080/geoserver/ATE_Workspace/wms',
+                    {
+                    layers: 'Raster',
+                    format:'image/png',
+                    transparent: 'true',
+                    version:'1.1.1'
+    } 
+    );
+    
+    var lyrPRG_Szkoly = L.tileLayer.wms('http://localhost:8080/geoserver/ATE_Workspace/wms',
+                    {
+                    layers: 'Raster',
+                    format:'image/png',
+                    transparent: 'true',
+                    version:'1.1.1'
+    } 
+    );
+    
+    var lyrPRG_Wiezenia = L.tileLayer.wms('http://localhost:8080/geoserver/ATE_Workspace/wms',
+                    {
+                    layers: 'Raster',
+                    format:'image/png',
+                    transparent: 'true',
+                    version:'1.1.1'
+    } 
+    );
+    
+    var lyrPRG_Cmentarze = L.tileLayer.wms('http://localhost:8080/geoserver/ATE_Workspace/wms',
+                    {
+                    layers: 'cmentarze',
+                    format:'image/png',
+                    transparent: 'true',
+                    version:'1.1.1'
+    } 
+    );
+    
+    var lyrPRG_Rekreacja = L.tileLayer.wms('http://localhost:8080/geoserver/ATE_Workspace/wms',
+                    {
+                    layers: 'Raster',
+                    format:'image/png',
+                    transparent: 'true',
+                    version:'1.1.1'
+    } 
+    );
+    
+    var lyrPRG_Mieszkaniowe = L.tileLayer.wms('http://localhost:8080/geoserver/ATE_Workspace/wms',
+                    {
+                    layers: 'Raster',
+                    format:'image/png',
+                    transparent: 'true',
+                    version:'1.1.1'
+    } 
+    );
+    
+    var lyrPRG_Najlep_szkoly = L.tileLayer.wms('http://localhost:8080/geoserver/ATE_Workspace/wms',
+                    {
+                    layers: 'Raster',
+                    format:'image/png',
+                    transparent: 'true',
+                    version:'1.1.1'
+    } 
+    );
+    
+    var lyrPRG_Szkoly = L.tileLayer.wms('http://localhost:8080/geoserver/ATE_Workspace/wms',
+                    {
+                    layers: 'Raster',
+                    format:'image/png',
+                    transparent: 'true',
+                    version:'1.1.1'
+    } 
+    );
+    
     var lyrOSM = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
     mymap.addLayer(lyrORTO);
     
@@ -50,10 +149,19 @@ $(document).ready(function(){
     
     //lista obiektów typucheckbox
     
-    var overlays = {"Wykaz Wojewodztw": lyrPRGWOJ};
-    
-    
-    
+    var overlays = {"Wykaz Wojewodztw": lyrPRGWOJ,
+                    "Wody liniowe": lyrPRGWody_liniowe,
+                    "Wody powierzchniowe": lyrPRGWody_powierz,
+                    "Drogi": lyrPRG_Drogi,
+                    "Kolej": lyrPRG_Kolej,
+                    "Szkoły": lyrPRG_Szkoly,
+                    "Więzienia": lyrPRG_Wiezenia,
+                    "Cmentarze": lyrPRG_Cmentarze,
+                    "Tereny rekreacyjne": lyrPRG_Rekreacja,
+                    "Osiedla mieszkaniowe": lyrPRG_Mieszkaniowe,
+                    "Szkoły w gminie": lyrPRG_Szkoly,
+                    "Najlepsza lokalizacja szkół": lyrPRG_Najlep_szkoly                    
+                   };
     
     
 
